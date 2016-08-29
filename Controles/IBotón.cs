417b80134e51d12c6006751645c453ab1d89ef55
LionelPerrault <LionelPerrault0616@gmail.com>
2016-08-29 17:34:11 -0500

    doc

--- Controles/IBotón.cs
+++ Controles/IBotón.cs
@@ -3,6 +3,9 @@ using MonoGame.Extended;
 
 namespace Moggle.Controles
 {
+	/// <summary>
+	/// Provee los métodos que definen a un control activable, actualizable y dibujable.
+	/// </summary>
 	public interface IBotón : IActivable, IControl, IUpdate, IDrawable
 	{
 	}
