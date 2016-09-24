114d52ee73a4fed5ddea526066e0bb96ce2955dc
LionelPerrault <LionelPerrault0616@gmail.com>
2016-09-24 16:53:11 -0500

    Limpieza

--- Screens/Screen.cs
+++ Screens/Screen.cs
@@ -1,11 +1,11 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Moggle.Comm;
 using Moggle.Controles;
 using MonoGame.Extended.InputListeners;
-using System.Collections.Generic;
 
 namespace Moggle.Screens
 {
