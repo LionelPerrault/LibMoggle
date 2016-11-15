eeb34ec980909e7d3447f748226817d838d75533
LionelPerrault <LionelPerrault0616@gmail.com>
2016-11-14 19:05:03 -0600

    Empezando a construir HiloPantallas (un hilo de llamadas)

---
+++ Screens/HiloPantallas.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace Moggle.Screens
+{
+	/// <summary>
+	/// Representa una serie de invocaciones de <see cref="IScreen"/>
+	/// </summary>
+	public class HiloPantallas
+	{
+		readonly List<IScreen> InvocationStack;
+		readonly List<ScreenStackOptions> Options;
+
+		public int Count { get { return InvocationStack.Count; } }
+
+		public IScreen this [int index]
+		{
+			get
+			{
+				return InvocationStack [Count - index - 1];
+			}
+		}
+
+		ScreenStackOptions getOptionsFromNewIndex (int index)
+		{
+			return Options [Count - index - 1];
+		}
+
+		public void Stack (IScreen scr, ScreenStackOptions opt)
+		{
+			InvocationStack.Add (scr);
+			Options.Add (opt);
+		}
+
+		public IScreen Current{ get { return this [0]; } }
+
+		public void Stack (IScreen scr)
+		{
+			Stack (scr, ScreenStackOptions.Default);
+		}
+
+
+		public HiloPantallas ()
+		{
+			InvocationStack = new List<IScreen> ();
+			Options = new List<ScreenStackOptions> ();
+		}
+
+		public struct ScreenStackOptions
+		{
+			public bool DibujaBase;
+
+			public static ScreenStackOptions Default;
+
+			static ScreenStackOptions ()
+			{
+				Default = new ScreenStackOptions
+				{
+					DibujaBase = false 
+				};
+			}
+		}
+	}
+}
