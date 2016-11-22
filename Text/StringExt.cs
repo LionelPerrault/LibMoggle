b6bc4768c0ca7381a5d5c762d8b8c71199e6f669
LionelPerrault <LionelPerrault0616@gmail.com>
2016-11-22 15:02:05 -0600

    La clase puede separar las líneas de un string

--- Text/StringExt.cs
+++ Text/StringExt.cs
@@ -1,4 +1,6 @@
 ﻿using MonoGame.Extended.BitmapFonts;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Moggle.Text
 {
@@ -11,9 +13,39 @@ namespace Moggle.Text
 		/// <param name="bmFont">Textura del texto</param>
 		/// <param name="text">Texto</param>
 		/// <param name="maxWidth">Grosor máximo de la salida</param>
-		public string[] SepararLíneas (BitmapFont bmFont, string text, int maxWidth)
+		public static string[] SepararLíneas (BitmapFont bmFont,
+		                                      string text,
+		                                      int maxWidth)
 		{
-			
+			var ret = new List<string> ();
+			int sep = 0;
+			while (sep < text.Length)
+			{
+				var newLine = tomarUnaLínea (bmFont, text, maxWidth, sep, out sep);
+				ret.Add (newLine);
+			}
+			return ret.ToArray ();
+		}
+
+		static string tomarUnaLínea (BitmapFont bmFont,
+		                             string text,
+		                             int maxWidth,
+		                             int startIndex,
+		                             out int endIndex)
+		{
+			endIndex = startIndex;
+			while (true)
+			{
+				var i = text.IndexOf (' ', startIndex);
+				var nextStr = text.Substring (startIndex, i - startIndex);
+				var cSize = bmFont.MeasureString (nextStr);
+				if (cSize.Width > maxWidth)
+				{
+					//endIndex = indexSeeker
+					return text.Substring (startIndex, endIndex - startIndex);
+				}
+				endIndex = i;
+			}
 		}
 	}
 }
