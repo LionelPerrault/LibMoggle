b96e51e10651e9ba5ff13ba01e6f02d02d6ac495
LionelPerrault <LionelPerrault0616@gmail.com>
2016-11-22 14:51:06 -0600

    Agregar clase de extensión de textos

---
+++ Text/StringExt.cs
@@ -0,0 +1,19 @@
+﻿using MonoGame.Extended.BitmapFonts;
+
+namespace Moggle.Text
+{
+	public static class StringExt
+	{
+		/// <summary>
+		/// Separa las líneas de texto para que quepan en un espacio horizontal dado
+		/// </summary>
+		/// <returns>Un arreglo de <c>string</c>que contiene los renglones del texto</returns>
+		/// <param name="bmFont">Textura del texto</param>
+		/// <param name="text">Texto</param>
+		/// <param name="maxWidth">Grosor máximo de la salida</param>
+		public string[] SepararLíneas (BitmapFont bmFont, string text, int maxWidth)
+		{
+			
+		}
+	}
+}
